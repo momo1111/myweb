4b5ad45d838c47f3fbebf8a91b17dce3d30df54a
liumengying <361838752@qq.com>
2019-12-25 15:46:35 +0800

    url

--- myvue/config/index.js
+++ myvue/config/index.js
@@ -16,6 +16,7 @@ module.exports = {
     host: 'localhost', // can be overwritten by process.env.HOST
     port: 5000, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
+    disableHostCheck: true,
     errorOverlay: true,
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
